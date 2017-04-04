2c4c0b2db47b0fbf80c3ddb112290a5ecb901bcf
radoikoff <radoikoff@gmail.com>
2017-04-05 00:00:55 +0300

    Updated IssueState model to have navigation property for list of issues

--- IssueTracker/IssueTracker/Models/IssueState.cs
+++ IssueTracker/IssueTracker/Models/IssueState.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,23 @@ namespace IssueTracker.Models
 {
     public class IssueState
     {
+        private ICollection<Issue> issues;
+
+        public IssueState()
+        {
+            this.issues = new HashSet<Issue>();
+        }
+        
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string State { get; set; }
+
+        public virtual ICollection<Issue> Issues
+        {
+            get { return this.issues; }
+            set { this.issues = value; }
+        }
     }
 }
